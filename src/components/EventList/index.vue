32ccfe1f62929b2b012ae3f17f4849ea4e99277c
inatatsu-tatsuhiro <tatsuhiro.9699@gmail.com>
2021-11-12 16:21:38 +0900

    :+1: イベント一覧、イベントページ

---
+++ src/components/EventList/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="center">
+      <u>
+        <h1>イベント一覧</h1>
+      </u>
+    </div>
+    <div class="center">
+      <div class="card2">
+        <div class="center">
+          <form v-on:submit="$router.push('/eventpage')">
+            <button>ブロックチェーンプロジェクト</button>
+          </form>
+          <form v-on:submit="$router.push('/eventpage')">
+            <button>ビジネスマナー講座</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  props: {
+    user: Object
+  }
+};
+
+
+</script>
+
+<style>
+
+.card2 {
+  width: 400px;
+  height: 400px;
+  border-radius: 25px;
+  background: #aaaaaa;
+}
+.center {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,32 +4,36 @@ import Tabs from '../views/Tabs.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: '/tabs/tab1'
+    path: "/",
+    redirect: "/tabs/tab1",
   },
   {
-    path: '/tabs/',
+    path: "/tabs/",
     component: Tabs,
     children: [
       {
-        path: '',
-        redirect: '/tabs/tab1'
+        path: "",
+        redirect: "/tabs/tab1",
       },
       {
-        path: 'tab1',
-        component: () => import('@/views/Tab1.vue')
+        path: "tab1/",
+        component: () => import("@/views/Tab1.vue"),
       },
       {
-        path: 'tab2',
-        component: () => import('@/views/Tab2.vue')
+        path: "tab2",
+        component: () => import("@/views/Tab2.vue"),
       },
       {
-        path: 'tab3',
-        component: () => import('@/views/Tab3.vue')
-      }
-    ]
-  }
-]
+        path: "tab3",
+        component: () => import("@/views/Tab3.vue"),
+      },
+      {
+        path: "tab4",
+        component: () => import("@/views/Tab4.vue"),
+      },
+    ],
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),

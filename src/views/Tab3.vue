955f9bdbc4a4386cf0a74a6f4a6f24eee6579f7b
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2021-11-12 15:46:28 +0900

    test1

--- src/views/Tab3.vue
+++ src/views/Tab3.vue
@@ -2,17 +2,17 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 3</ion-title>
+        <ion-title>イベント作成</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 3</ion-title>
+          <ion-title size="large">イベント作成</ion-title>
         </ion-toolbar>
       </ion-header>
       
-      <ExploreContainer name="Tab 3 page" />
+      <ExploreContainer name="イベント作成" />
     </ion-content>
   </ion-page>
 </template>

e97b5724e8930e8fa3814998f5d12dd7e84ea563
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2021-11-12 16:21:53 +0900

    EventCreate and ScoreCheck

--- src/views/Tab3.vue
+++ src/views/Tab3.vue
@@ -11,18 +11,30 @@
           <ion-title size="large">イベント作成</ion-title>
         </ion-toolbar>
       </ion-header>
-      
-      <ExploreContainer name="イベント作成" />
+      <EventCreate />
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+  import {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+  } from "@ionic/vue";
+  import EventCreate from "@/components/EventCreate.vue";
 
-export default  {
-  name: 'Tab3',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-}
+  export default {
+    name: "Tab3",
+    components: {
+      EventCreate,
+      IonHeader,
+      IonToolbar,
+      IonTitle,
+      IonContent,
+      IonPage,
+    },
+  };
 </script>

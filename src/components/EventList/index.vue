21cf373683ffb8f757839453910c8c3f11ab1468
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2021-11-12 16:37:03 +0900

    test

--- src/components/EventList/index.vue
+++ src/components/EventList/index.vue
@@ -8,46 +8,41 @@
     <div class="center">
       <div class="card2">
         <div class="center">
-          <form v-on:submit="$router.push('/tabs/tab4')">
-            <button>ブロックチェーンプロジェクト</button>
-          </form>
-          <form v-on:submit="$router.push('/eventpage')">
-            <button>ビジネスマナー講座</button>
-          </form>
+          <ion-button href="/tabs/tab4">ブロックチェーンプロジェクト</ion-button>
+          <ion-button href="/tabs/tab4">ビジネスマナー講座</ion-button>
         </div>
       </div>
     </div>
   </div>
-  
 </template>
 
 <script>
-export default {
-  props: {
-    user: Object
-  }
-};
-
-
+  import {
+    IonButton,
+  } from "@ionic/vue";
+  import { defineComponent } from "vue";
+  export default defineComponent({
+    components: { IonButton },
+  });
 </script>
 
 <style>
-.container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 
-.card2 {
-  width: 400px;
-  height: 400px;
-  border-radius: 25px;
-  background: #aaaaaa;
-}
-.center {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+  .card2 {
+    width: 400px;
+    height: 400px;
+    border-radius: 25px;
+    background: #aaaaaa;
+  }
+  .center {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 </style>
